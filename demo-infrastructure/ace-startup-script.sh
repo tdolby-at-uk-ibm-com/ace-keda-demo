1b170dc2d7fea20816d144d955f2fafc4e299a38
Trevor Dolby <trevor.dolby@ibm.com>
2024-11-06 16:38:53 -0600

    Container startup fix 1
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- demo-infrastructure/ace-startup-script.sh
+++ demo-infrastructure/ace-startup-script.sh
@@ -8,25 +8,15 @@ echo "Pulling in secrets"
 
 set +x
 
-mkdir /home/aceuser/ace-server/run/DefaultPolicies
-echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns2:policyProjectDescriptor xmlns="http://com.ibm.etools.mft.descriptor.base" xmlns:ns2="http://com.ibm.etools.mft.descriptor.policyProject"><references/></ns2:policyProjectDescriptor>' > /home/aceuser/ace-server/run/DefaultPolicies/policy.descriptor
-
-export TEMPLATE_POLICYXML=/tmp/MQoC.policyxml
-
-if [[ -e "/home/aceuser/ace-server/MQoC.policyxml" ]]
-then
-    # Maven s2i
-    export TEMPLATE_POLICYXML=/home/aceuser/ace-server/MQoC.policyxml
-fi
+export TEMPLATE_POLICYXML=/home/aceuser/ace-server/run/DefaultPolicies/MQoC.policyxml
 
 echo "policy ${TEMPLATE_POLICYXML} before"
 cat ${TEMPLATE_POLICYXML}
-sed -i "s/HOSTNAME/`cat /run/secrets/mq/hostName`/g" ${TEMPLATE_POLICYXML}
-sed -i "s/00000/`cat /run/secrets/mq/portNumber`/g" ${TEMPLATE_POLICYXML}
+sed -i "s|<queueManagerHostname>.*</queueManagerHostname>|<queueManagerHostname>`cat /run/secrets/mq/hostName`</queueManagerHostname>|g" ${TEMPLATE_POLICYXML}
+sed -i "s|<listenerPortNumber>.*</listenerPortNumber>|<listenerPortNumber>`cat /run/secrets/mq/portNumber`</listenerPortNumber>|g" ${TEMPLATE_POLICYXML}
 
 echo "policy ${TEMPLATE_POLICYXML} after"
 cat ${TEMPLATE_POLICYXML}
-cp ${TEMPLATE_POLICYXML} /home/aceuser/ace-server/run/DefaultPolicies/
 
 mqsisetdbparms -w /home/aceuser/ace-server -n mq::MQoC -u `cat /run/secrets/mq/USERID` -p `cat /run/secrets/mq/PASSWORD`
 
